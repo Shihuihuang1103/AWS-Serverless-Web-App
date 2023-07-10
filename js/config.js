fafe645fe1408ab1bb2bd7b2d091b796dfe98938
Shihui Huang <sh4480@nyu.edu>
2023-07-10 16:31:33 -0400

    cognito

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_0H6C7BeSq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2tlk7vpb6tq81q4mutu7uu5sob', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://main.d1clmolarz2ig3.amplifyapp.com/register.html' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
