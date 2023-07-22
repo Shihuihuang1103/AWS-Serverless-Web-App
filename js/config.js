15a931c36f11b9f51b9a9ba7d2b5f138d23b688d
Shihui Huang <sh4480@nyu.edu>
2023-07-22 16:52:37 -0400

    new_configuration

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://main.d1clmolarz2ig3.amplifyapp.com/register.html' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://tu0hj3rrd4.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
